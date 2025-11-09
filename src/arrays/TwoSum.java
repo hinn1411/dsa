729efd1de65d5c7984770d914098a24c32c29afd
Hien Giang <quachtrieuvu@gmail.com>
2025-11-09 13:53:20 +0700

    feat(arrays): two-sum (#1) - hashmap O(n)

--- src/arrays/TwoSum.java
+++ src/arrays/TwoSum.java
@@ -1,17 +1,41 @@
 package arrays;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class TwoSum {
-    public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            int want = target - nums[i];
-            if (map.containsKey(want)) {
-                return new int[]{map.get(want), i};
-            }
-            map.put(nums[i], i);
+  /*
+    Time: O(n^2)
+    Space: O(1)
+   */
+  public int[] bruteForce(int[] nums, int target) {
+    int n = nums.length;
+    for (int i = 0; i < n - 1; i++) {
+      for (int j = i + 1; j < n; j++) {
+        if (nums[i] + nums[j] == target) {
+          return new int[]{i, j};
         }
-        return new int[]{-1, -1};
+      }
     }
+    return new int[]{-1, -1};
+  }
+
+  public int[] twoSum(int[] nums, int target) {
+    HashMap<Integer, Integer> seenElements = new HashMap<>();
+    for (int i = 0; i < nums.length; i++) {
+      int current = nums[i];
+      int complement = target - current;
+      if (seenElements.containsKey(complement)) {
+        return new int[]{seenElements.get(complement), i};
+      }
+      seenElements.put(nums[i], i);
+    }
+    return new int[]{-1, -1};
+  }
+
+  public static void main(String[] args) {
+    TwoSum s = new TwoSum();
+    System.out.println(Arrays.toString(s.bruteForce(new int[]{3, -3}, 0)));
+    System.out.println(Arrays.toString(s.twoSum(new int[]{3 ,-3}, 0)));
+  }
 }
