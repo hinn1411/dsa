16e9c043adf6cbc750061cd3ba1221f7185f14f0
Hien Giang <quachtrieuvu@gmail.com>
2025-11-13 23:53:51 +0700

    feat(strings): valid anagram - Hashmap O(n)

--- src/arrays/ValidAnagram.java
+++ src/arrays/ValidAnagram.java
@@ -30,10 +30,36 @@ public class ValidAnagram {
     return true;
   }
 
+  public boolean isAnagram_D2(String s, String t) {
+    int sLen = s.length(), tLen = t.length();
+    if (sLen != tLen) {
+      return false;
+    }
+
+    Map<Character, Integer> sMap = new HashMap<>();
+    Map<Character, Integer> tMap = new HashMap<>();
+
+    for (int i = 0; i < sLen; i++) {
+      sMap.put(s.charAt(i), sMap.getOrDefault(s.charAt(i), 0) + 1);
+      tMap.put(t.charAt(i), tMap.getOrDefault(t.charAt(i), 0) + 1);
+    }
+
+    for (char c: sMap.keySet()) {
+      if (!sMap.get(c).equals(tMap.get(c))) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
   public static void main(String[] args) {
     ValidAnagram v = new ValidAnagram();
-    System.out.println(v.isAnagram("car", "rac"));
-    System.out.println(v.isAnagram("car", "cxr"));
-    System.out.println(v.isAnagram("ccr", "ccc"));
+//    System.out.println(v.isAnagram("car", "rac"));
+//    System.out.println(v.isAnagram("car", "cxr"));
+//    System.out.println(v.isAnagram("ccr", "ccc"));
+    System.out.println(v.isAnagram_D2("car", "rac"));
+    System.out.println(v.isAnagram_D2("car", "cxr"));
+    System.out.println(v.isAnagram_D2("ccr", "c"));
   }
 }
