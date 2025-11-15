bef5ba5c23c30b1d84696b9af3769334663fc2ac
Hien Giang <quachtrieuvu@gmail.com>
2025-11-16 00:08:54 +0700

    feat(strings): longest substring without repeating characters - set O(n)

--- src/strings/LongestSubstringWithoutRepeatingCharacters.java
+++ src/strings/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,7 +1,9 @@
 package strings;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class LongestSubstringWithoutRepeatingCharacters {
   public int lengthOfLongestSubstring(String s) {
@@ -18,12 +20,32 @@ public class LongestSubstringWithoutRepeatingCharacters {
     }
     return maxLen;
   }
+
+  public int lengthOfLongestSubstring_D2(String s) {
+    int res = 0, l = 0, len = s.length();
+    Set<Character> seen = new HashSet<>();
+
+    for (int r = 0; r < len; r++) {
+      char cur = s.charAt(r);
+
+      while(seen.contains(cur)) {
+        char begin = s.charAt(l);
+        seen.remove(begin);
+        l++;
+      }
+
+      seen.add(cur);
+      res = Math.max(res, r - l + 1);
+    }
+    return res;
+  }
   public static void main (String[] args) {
     LongestSubstringWithoutRepeatingCharacters l = new LongestSubstringWithoutRepeatingCharacters();
-//    System.out.println(l.lengthOfLongestSubstring(""));
-//    System.out.println(l.lengthOfLongestSubstring("a"));
-//    System.out.println(l.lengthOfLongestSubstring("aaaa"));
-//    System.out.println(l.lengthOfLongestSubstring("aabbcc"));
-    System.out.println(l.lengthOfLongestSubstring("aabcad"));
+    System.out.println(l.lengthOfLongestSubstring_D2(""));
+    System.out.println(l.lengthOfLongestSubstring_D2("a"));
+    System.out.println(l.lengthOfLongestSubstring_D2("aaaa"));
+    System.out.println(l.lengthOfLongestSubstring_D2("aabbcc"));
+    System.out.println(l.lengthOfLongestSubstring_D2("aabcad"));
+    System.out.println(l.lengthOfLongestSubstring_D2("abcdadefvdf"));
   }
 }
