580b651fc391818491973ecf6c851b9373a114e2
Hien Giang <quachtrieuvu@gmail.com>
2026-01-13 22:15:27 +0700

    feat(tree): bfs  - O(n) in time & O(h) in space

--- src/tree/MaximumDepthOfBinaryTree.java
+++ src/tree/MaximumDepthOfBinaryTree.java
@@ -1,5 +1,8 @@
 package tree;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 public class MaximumDepthOfBinaryTree {
   public class TreeNode {
     int val;
@@ -27,6 +30,29 @@ public class MaximumDepthOfBinaryTree {
     int leftHeight = maxDepth(root.left);
     int rightHeight = maxDepth(root.right);
     return Math.max(leftHeight, rightHeight) + 1;
+  }
+
+  public int maxDepth_Dfs(TreeNode root) {
+    if (root == null) {
+      return 0;
+    }
+    Queue<TreeNode> q = new ArrayDeque<>();
+    q.add(root);
+    int depth = 0;
+    while (!q.isEmpty()) {
+      int currentTotalNodes = q.size();
+      for (int i = 0; i < currentTotalNodes; i++) {
+        TreeNode current = q.poll();
+        if (current != null && current.left != null) {
+          q.add(current.left);
+        }
+        if (current != null && current.right != null) {
+          q.add(current.right);
+        }
+      }
+      depth++;
+    }
+    return depth;
   }
 
   public static void main(String[] args) {
