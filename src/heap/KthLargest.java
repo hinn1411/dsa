7b5df9c653b91fe61ccf7eed291d2ad5c2aa36c7
Hien Giang <quachtrieuvu@gmail.com>
2026-01-28 23:54:19 +0700

    feat(heap): recursion - O(n) in time & O(h) in space

---
+++ src/heap/KthLargest.java
@@ -0,0 +1,38 @@
+package heap;
+
+import java.util.PriorityQueue;
+
+public class KthLargest {
+
+  PriorityQueue<Integer> q;
+  int k;
+  public KthLargest(int k, int[] nums) {
+    this.k = k;
+    q = new PriorityQueue<>();
+    for (int num : nums) {
+      q.add(num);
+      if (q.size() > k) {
+        q.poll();
+      }
+    }
+  }
+
+  public int add(int val) {
+    q.add(val);
+    if (q.size() > k) {
+      q.poll();
+    }
+    return q.peek();
+  }
+
+  public static void main(String[] args) {
+    KthLargest k = new KthLargest(3, new int[]{4, 5, 8, 2});
+    System.out.println(k.add(3));
+    System.out.println(k.add(5));
+    System.out.println(k.add(10));
+    System.out.println(k.add(9));
+    System.out.println(k.add(4));
+
+
+  }
+}
