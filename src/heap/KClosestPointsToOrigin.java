80a6fd8cfb0fb09087f17d0e6d360a8a48b41ef0
Hien Giang <quachtrieuvu@gmail.com>
2026-01-31 14:38:59 +0700

    feat(heap): sort - O(nlogn) in time & O(n) in space

--- src/heap/KClosestPointsToOrigin.java
+++ src/heap/KClosestPointsToOrigin.java
@@ -1,6 +1,8 @@
 package heap;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.PriorityQueue;
 
 public class KClosestPointsToOrigin {
@@ -30,11 +32,25 @@ public class KClosestPointsToOrigin {
     }
     return res;
   }
+  public int[][] kClosest_Sort(int[][] points, int k) {
+    List<int[]> sortedPoints = Arrays.stream(points).sorted((a, b) -> Integer.compare(getDistance(a), getDistance(b)))
+        .toList();
+    int[][] res = new int[k][];
+    for (int i = 0; i < k; i++) {
+      res[i] = new int[] { sortedPoints.get(i)[0], sortedPoints.get(i)[1] };
+    }
+    return res;
+  }
+
+  private int getDistance(int[] point) {
+    return point[0] * point[0] + point[1] * point[1];
+  }
+
   public static void main(String[] args) {
     KClosestPointsToOrigin k = new KClosestPointsToOrigin();
-    int[][] arr1 = {{-2, 2}};
-    System.out.println(Arrays.deepToString(k.kClosest(arr1, 1))); // [[-2, 2]]
+//    int[][] arr1 = {{-2, 2}};
+//    System.out.println(Arrays.deepToString(k.kClosest(arr1, 1))); // [[-2, 2]]
     int[][] arr2 = {{3,3}, {5,-1}, {-2,4}};
-    System.out.println(Arrays.deepToString(k.kClosest(arr2, 2))); // [[3,3], [-2, 4]]
+    System.out.println(Arrays.deepToString(k.kClosest_Sort(arr2, 2))); // [[3,3], [-2, 4]]
   }
 }
