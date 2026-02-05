dea9a6c4bdfd199d51390d3e0e57c434da7c7188
Hien Giang <quachtrieuvu@gmail.com>
2026-02-05 23:53:37 +0700

    feat(interval): sorting - O(nlogn) in time and O(1) in space

---
+++ src/internal/MeetingRooms.java
@@ -0,0 +1,34 @@
+package internal;
+
+import java.util.List;
+
+public class MeetingRooms {
+  public class Interval {
+    public int start, end;
+
+    public Interval(int start, int end) {
+      this.start = start;
+      this.end = end;
+    }
+  }
+
+  public boolean canAttendMeetings(List<Interval> intervals) {
+    if (intervals.size() <= 1) {
+      return true;
+    }
+    intervals.sort((a, b) -> Integer.compare(a.start, b.start));
+    int n = intervals.size();
+    for (int i = 0; i < n - 1; i++) {
+      Interval former = intervals.get(i);
+      Interval latter = intervals.get(i + 1);
+      if (former.end > latter.start) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public static void main(String[] args) {
+
+  }
+}
