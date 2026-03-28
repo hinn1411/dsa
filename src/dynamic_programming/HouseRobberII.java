a18e96b0889d2dd5adeffa7032bad2dd109d4430
Hien Giang <quachtrieuvu@gmail.com>
2026-03-28 15:35:11 +0700

    feat(dp): tabulation - O(n) in time & O(n) in space

--- src/dynamic_programming/HouseRobberII.java
+++ src/dynamic_programming/HouseRobberII.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class HouseRobberII {
 
-  public int rob(int[] nums) {
+  public int rob_Memoization(int[] nums) {
     int n = nums.length;
     if (n == 1) {
       return nums[0];
@@ -33,5 +33,29 @@ public class HouseRobberII {
         helper(nums, start, cur - 1, memo)
     );
     return memo[cur];
+  }
+
+  public int rob(int[] nums) {
+    int n = nums.length;
+    if (n == 1) {
+      return nums[0];
+    }
+
+    return Math.max(
+      compute(nums, 0, n - 2),
+      compute(nums, 1, n - 1));
+  }
+
+  private int compute(int[] nums, int start, int end) {
+    if (start == end) {
+      return nums[start];
+    }
+    int[] dp = new int[nums.length];
+    dp[start] = nums[start];
+    dp[start + 1] = Math.max(nums[start], nums[start + 1]);
+    for (int i = start + 2; i <= end; i++) {
+      dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1]);
+    }
+    return dp[end];
   }
 }
