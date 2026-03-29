a2fa1c0da95d48f26910acc4dff5fbfd68610526
Hien Giang <quachtrieuvu@gmail.com>
2026-03-29 15:19:47 +0700

    feat(dp): tabulation - O(n) in time & O(n) in space

--- src/dynamic_programming/CoinChange.java
+++ src/dynamic_programming/CoinChange.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class CoinChange {
   private final int INF = 10000 + 1;
 
-  public int coinChange(int[] coins, int amount) {
+  public int coinChange_Memoization(int[] coins, int amount) {
     int[] memo = new int[amount + 1];
     Arrays.fill(memo, -1);
     int min = helper(coins, amount, memo);
@@ -30,4 +30,21 @@ public class CoinChange {
     memo[remaining] = minCoins;
     return memo[remaining];
   }
+
+  public int coinChange(int[] coins, int amount) {
+    int[] dp = new int[amount + 1];
+    Arrays.fill(dp, INF);
+    dp[0] = 0;
+    for (int i = 1; i <= amount; i++) {
+      int remaining;
+      for (int coin: coins) {
+        remaining = i - coin;
+        if (remaining >= 0) {
+          dp[i] = Math.min(dp[i], dp[remaining] + 1);
+        }
+      }
+    }
+    return dp[amount] == INF ? -1 : dp[amount];
+  }
+
 }
